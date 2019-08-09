1644533de2e0f995078da166722fbe323bcff63b
zempo <zelenkosolomon@gmail.com>
2019-08-09 11:12:28 -0700

    finalize error page styling

--- src/components/Utils/ErrorPage.js
+++ src/components/Utils/ErrorPage.js
@@ -7,14 +7,14 @@ import "./css/Error.css";
 class ErrorPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { error: true, info: "server is down" };
+    this.state = { error: null, info: null };
   }
 
   static contextType = UserContext;
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { error };
   }
 
   componentDidCatch(error, info) {
@@ -26,7 +26,6 @@ class ErrorPage extends Component {
     if (this.state.info) {
       return (
         <div className="error-bounds">
-          <div>{this.state.error && this.state.error.toString()}</div>
           <div className="list-card-err">
             <input type="checkbox" id={`err-card-toggle-1`} className="err-card-toggle" value="selected" />
             <label className="err-card-container" htmlFor={`err-card-toggle-1`}>
@@ -37,8 +36,8 @@ class ErrorPage extends Component {
               </div>
               <div className="inner-left-err face-err">
                 <p>
-                  We're sorry for the inconvenience this might have caused you. We'll get to work so that we can play
-                  our cards right.
+                  We're sorry for the inconvenience this might have caused you. We'll work to get our server running so
+                  that we might play our cards right.
                 </p>
               </div>
               <div className="inner-right-err face-err">
