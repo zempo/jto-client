5e3cb29c24cf2dd50af5bd4f80aff0323db72fa7
zempo <zelenkosolomon@gmail.com>
2019-09-20 22:07:58 -0700

    allow reload on delete or publish change

--- src/components/Forms/Card/DeleteUserCard.js
+++ src/components/Forms/Card/DeleteUserCard.js
@@ -1,51 +1,22 @@
-import React, { useState, useContext, useLayoutEffect, useRef } from "react";
+import React, { useState } from "react";
 import { JtoNotification } from "../../Utils/Utils";
-import { deleteUserCard, listUserCards } from "../../../services/endpoints-service";
-import { CardsContext } from "../../../contexts/CardsContext";
+import { deleteUserCard } from "../../../services/endpoints-service";
 
 const DeleteUserCard = ({ item, cancel }) => {
   // eslint-disable-next-line
-  const [displaying, setDisplaying] = useState(true);
   const [error, setError] = useState(false);
   const [resMsg, setResMsg] = useState("");
   const [resStatus, setResStatus] = useState(0);
-  const unmounted = useRef(false);
-  const {
-    value: { moveOrDeleteCard }
-  } = useContext(CardsContext);
-
-  useLayoutEffect(() => {
-    // unmounted.current = false;
-    return () => {
-      // unmounted.current = true;
-      // // console.clear();
-      // setTimeout(() => {
-      //   console.clear();
-      //   console.log("dismount");
-      //   unmounted.current = true;
-      // }, 1000);
-    };
-  }, []);
 
   const handleDelete = async (e) => {
     setResStatus(0);
     setResMsg("");
     try {
       let deleted = await deleteUserCard.delete(`/${item}`);
-      let newCards = await listUserCards.get("");
-      let updatedCards = await moveOrDeleteCard(newCards.data, newCards.data);
 
-      // return array of all cards at present
-      // set a variable equal to all cards minus the id of current
-      // delete
-      setResStatus(204);
+      setResStatus(deleted.status);
       setResMsg("Occasion Deleted");
-      cancel();
-      setTimeout(() => {
-        // setResStatus(0);
-        // unmounted.current = true;
-      }, 1000);
-      // window.location.reload();
+      window.location.reload();
     } catch (err) {
       setError(true);
       setResStatus(err.response.status);
@@ -57,7 +28,7 @@ const DeleteUserCard = ({ item, cancel }) => {
     <div className={resStatus === 0 || resStatus === 204 ? null : "shake"}>
       <h2>Are you sure you want to delete your occasion?</h2>
       <p>Once you delete an Occasion, this action cannot be undone.</p>
-      {/* {resStatus === 0 ? null : <JtoNotification type={resStatus} msg={resMsg} />} */}
+      {resStatus === 0 ? null : <JtoNotification type={resStatus} msg={resMsg} />}
       <button onClick={handleDelete}>Yes</button>
       <button className="close-modal" onClick={cancel}>
         X
