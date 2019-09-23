cf67fc13eff05c6e4f2fe42b51a68c41fc33aefd
zempo <zelenkosolomon@gmail.com>
2019-09-23 15:31:37 -0700

    update download controls

--- src/components/Routes/DownloadPage.js
+++ src/components/Routes/DownloadPage.js
@@ -13,8 +13,11 @@ class DownloadPage extends Component {
     super(props);
     this.state = {
       paperSize: "A5",
+      frontText: "#000000",
       frontBg: "#f8f5f5",
+      innerLeftText: "#000000",
       innerLeftBg: "#f8f5f5",
+      innerRightText: "#000000",
       innerRightBg: "#f8f5f5",
       border: "#f8f5f5",
       stroke: 5,
@@ -46,11 +49,33 @@ class DownloadPage extends Component {
   }
 
   render() {
-    let { frontBg, innerLeftBg, innerRightBg, border, stroke, size, card } = this.state;
+    let {
+      frontBg,
+      frontText,
+      innerLeftText,
+      innerLeftBg,
+      innerRightText,
+      innerRightBg,
+      border,
+      stroke,
+      size,
+      card
+    } = this.state;
     let { user: card_user } = this.context.value;
     let outputStyle = {
+      // border: `${stroke}px solid ${border}`,
+    };
+    let pageStyle = {
+      fontFamily: `${card.theme !== "" ? ThemeStyles[`${card.theme}`].all.fontFamily : "none"}`,
       border: `${stroke}px solid ${border}`,
-      fontFamily: `${card.theme !== "" ? ThemeStyles[`${card.theme}`].all.fontFamily : "none"}`
+      outline: `${stroke}px solid ${border}`,
+      outlineOffset: `-${stroke}px`,
+      height: `${
+        size === "A1" ? "480" : size === "A6" ? "576" : size === "A7" ? "672" : size === "A9" ? "816" : "830"
+      }px`,
+      width: `${
+        size === "A1" ? "336" : size === "A6" ? "348" : size === "A7" ? "480" : size === "A9" ? "528" : "600"
+      }px`
     };
 
     return (
@@ -61,18 +86,57 @@ class DownloadPage extends Component {
         </h2>
         <JtoSection className="download-controls">
           <form className="jto-download-form">
+            <fieldset className="download-text-color">
+              <legend>Text Color</legend>
+              <input
+                type="color"
+                id="frontText"
+                name="frontText"
+                defaultValue={frontText}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="frontText">Front</label>
+              <input
+                type="color"
+                id="innerLeftText"
+                name="innerLeftText"
+                defaultValue={innerLeftText}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="innerLeftText">Inner Left</label>
+              <input
+                type="color"
+                id="innerRightText"
+                name="innerRightText"
+                defaultValue={innerRightText}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="innerRightText">Inner Right</label>
+            </fieldset>
             <fieldset className="download-background">
-              <h3>Background</h3>
-              <input type="color" id="frontBg" name="frontBg" defaultValue={frontBg} />
+              <legend>Background</legend>
+              <input type="color" id="frontBg" name="frontBg" defaultValue={frontBg} onChange={this.handleChange} />
               <label htmlFor="frontBg">Front</label>
-              <input type="color" id="innerLeftBg" name="innerLeftBg" defaultValue={innerLeftBg} />
+              <input
+                type="color"
+                id="innerLeftBg"
+                name="innerLeftBg"
+                defaultValue={innerLeftBg}
+                onChange={this.handleChange}
+              />
               <label htmlFor="innerLeftBg">Inner Left</label>
-              <input type="color" id="innerRightBg" name="innerRightBg" defaultValue={innerRightBg} />
+              <input
+                type="color"
+                id="innerRightBg"
+                name="innerRightBg"
+                defaultValue={innerRightBg}
+                onChange={this.handleChange}
+              />
               <label htmlFor="innerRightBg">Inner Right</label>
             </fieldset>
             <fieldset className="download-border">
-              <h3>Border</h3>
-              <input type="color" id="border" name="border" defaultValue={border} />
+              <legend>Border</legend>
+              <input type="color" id="border" name="border" defaultValue={border} onChange={this.handleChange} />
               <label htmlFor="border">Border Color</label>
               <input
                 id="typeinp"
@@ -84,7 +148,9 @@ class DownloadPage extends Component {
                 onChange={this.handleChange}
                 step="1"
               />
-              <label htmlFor=""></label>
+              <label htmlFor="stroke" style={{ display: "inline-block" }}>
+                Border Width -- {stroke}
+              </label>
             </fieldset>
             <fieldset className="download-size">
               <label>
@@ -119,20 +185,50 @@ class DownloadPage extends Component {
           <PDFExport
             // createPdf={this.createPdf}
             forcePageBreak=".page-break"
-            paperSize={size}
+            paperSize="letter"
             fileName="my-occasion.pdf"
             margin="20px"
             ref={(component) => (this.pdfExportComponent = component)}
             style={outputStyle}
           >
-            <div className="export-pg export-front" style={{ backgroundColor: frontBg }}>
-              <h3>{card.front_message}</h3>
+            <div
+              className="export-pg export-front"
+              style={{
+                backgroundColor: frontBg,
+                color: frontText,
+                height: pageStyle.height,
+                width: pageStyle.width,
+                border: pageStyle.border,
+                fontFamily: pageStyle.fontFamily
+              }}
+            >
+              <h2>{card.front_message}</h2>
               <img src={card.front_image} alt="download-cover" />
             </div>
-            <div className="export-pg export-inside-left page-break" style={{ backgroundColor: innerLeftBg }}>
-              <p>{card.inside_message}</p>
+            <div
+              className="export-pg export-inside-left page-break"
+              style={{
+                backgroundColor: innerLeftBg,
+                color: innerLeftText,
+                height: pageStyle.height,
+                width: pageStyle.width,
+                border: pageStyle.border,
+                fontFamily: pageStyle.fontFamily
+              }}
+            >
+              <h3 className="download-text">{card.inside_message}</h3>
             </div>
-            <div className="export-pg export-inside-right page-break" style={{ backgroundColor: innerRightBg }}>
+            <div
+              className="export-pg export-inside-right page-break"
+              style={{
+                backgroundColor: innerRightBg,
+                color: innerRightText,
+                height: pageStyle.height,
+                width: pageStyle.width,
+                border: pageStyle.border,
+                fontFamily: pageStyle.fontFamily
+              }}
+            >
               <img src={card.inside_image} alt="download-inside-figure" />
             </div>
           </PDFExport>
