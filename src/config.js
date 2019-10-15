4089dd449745b608098315e7450677c4934c5545
zempo <zelenkosolomon@gmail.com>
2019-10-14 22:54:49 -0700

    fix fonts

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  // API_ENDPOINT: "http://localhost:8000/api",
   API_ENDPOINT: "https://jto-server.herokuapp.com/api",
   TOKEN_KEY: "jto-client-auth-token",
   TOKEN_USER: "jto-client-user"
